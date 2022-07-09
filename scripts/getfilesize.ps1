a4f196bc3fee653ee23c59c03c48b93d68bd1ba4
Bill Lien <bill.lien@elliemae.com>
2022-07-09 09:12:14 -0700

    update script

--- scripts/getfilesize.ps1
+++ scripts/getfilesize.ps1
@@ -1,12 +1,14 @@
 param(
   [Parameter(Mandatory=$true, HelpMessage="Enter the file path.")]
-  [String]$filepath
+  [String]$FilePath,
+  [Parameter(Mandatory=$true, HelpMessage="Enter the file path.")]
+  [int]$SizeInMB
 )
 
-$objFile = Get-ItemProperty -Path "$filepath"
-$filesize = $objFile.Length / 1GB
-if ($filesize -gt 0.5) {
+$objFile = Get-ItemProperty -Path "$FilePath"
+$filesize = $objFile.Length / 1MB
+if ($filesize -gt $SizeInMB) {
   $instanceid = (invoke-webrequest http://169.254.169.254/latest/meta-data/instance-id -UseBasicParsing).content
-  Write-Host ">>>> $('{0,-15} {1,-20} {2,-10} {3,-40}' -f $(hostname), $instanceid, $('{0:n2} GB' -f $filesize), $objFile.FullName)"
+  Write-Host ">>>> $('{0,-15} {1,-20} {2,-10} {3,-40}' -f $(hostname), $instanceid, $('{0:n2} MB' -f $filesize), $objFile.FullName)"
 }
 
